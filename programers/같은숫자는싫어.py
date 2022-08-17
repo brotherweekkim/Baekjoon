add0ac7124df8cec5d17574fc0825143b81ac958
Hyung Ju <ghj6282@hanmail.net>
2022-08-17 22:20:18 +0900

    220817 pro 같은숫자는싫어

---
+++ programers/같은숫자는싫어.py
@@ -0,0 +1,8 @@
+def solution(arr):
+    answer = []
+    answer.append(arr[0])
+    for num in arr[1:]:
+        if answer[-1] == num:
+            continue
+        answer.append(num)
+    return answer
