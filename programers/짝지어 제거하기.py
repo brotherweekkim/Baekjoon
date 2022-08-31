a9b78a4644f13f054d7f829067a03bbdca6f59ac
Hyung Ju <ghj6282@hanmail.net>
2022-08-31 23:03:51 +0900

    220831 pro 짝지어 제거하기

---
+++ programers/짝지어 제거하기.py
@@ -0,0 +1,19 @@
+def solution(s):    
+    if len(s) % 2:
+        return 0
+    
+    stack = []
+    
+    for i in s:
+        if stack:
+            if stack[-1] == i:
+                stack.pop()
+            else:
+                stack.append(i)
+        else:
+            stack.append(i)
+    
+    if stack:
+        return 0
+        
+    return 1
